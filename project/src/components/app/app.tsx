f59cb226e8ec4c8a1006f6f29995b6175ecf9c3a
Ruslan <ruslan.shkolnykh@protonmail.com>
2021-10-10 20:44:07 -0700

    added proptypes in main

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,7 +1,7 @@
 import Main from '../main/main';
 
 function App(): JSX.Element {
-  return <Main />;
+  return <Main title={'testTitle'} genre={'genre'} date={'2014'} />;
 }
 
 export default App;
--- project/src/components/main/main.tsx
+++ project/src/components/main/main.tsx
@@ -1,6 +1,12 @@
 import MovieItem from '../movie-item/movie-item';
 
-function Main(): JSX.Element {
+type PromoMovie = {
+    title: string,
+    genre: string,
+    date: string
+}
+
+function Main({title, genre, date}: PromoMovie): JSX.Element {
   return (
     <>
       <section className="film-card">
@@ -38,10 +44,10 @@ function Main(): JSX.Element {
             </div>
 
             <div className="film-card__desc">
-              <h2 className="film-card__title">The Grand Budapest Hotel</h2>
+              <h2 className="film-card__title">{title}</h2>
               <p className="film-card__meta">
-                <span className="film-card__genre">Drama</span>
-                <span className="film-card__year">2014</span>
+                <span className="film-card__genre">{genre}</span>
+                <span className="film-card__year">{date}</span>
               </p>
 
               <div className="film-card__buttons">
