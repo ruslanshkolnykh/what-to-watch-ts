7d9bd2e3a3260396a22b97e1129295631cf616bc
Ruslan <ruslan.shkolnykh@protonmail.com>
2021-10-31 16:19:29 -0700

    added SRC and poster in <video>

--- project/src/components/player/player.tsx
+++ project/src/components/player/player.tsx
@@ -7,29 +7,29 @@ type Props = {
 function Player({film}:Props): JSX.Element {
   return (
     <div className="player">
-      <video src={film.video_link} className="player__video" poster={film.poster_image} muted></video>
+      <video src={film.video_link} className="player__video" poster={film.poster_image} muted controls></video>
 
       <button type="button" className="player__exit">Exit</button>
 
       <div className="player__controls">
         <div className="player__controls-row">
           <div className="player__time">
-            <progress className="player__progress" value="30" max="100"></progress>
+            <progress className="player__progress" value={0} max={100}></progress>
             <div className="player__toggler" style={{left: '30%'}}>Toggler</div>
           </div>
           <div className="player__time-value">1:30:29</div>
         </div>
 
         <div className="player__controls-row">
-          <button type="button" className="player__play">
+          <button type="button" className="player__play" data-state="play">
             <svg viewBox="0 0 19 19" width="19" height="19">
               <use xlinkHref="#play-s"></use>
             </svg>
             <span>Play</span>
           </button>
-          <div className="player__name">Transpotting</div>
+          <div className="player__name">{film.name}</div>
 
-          <button type="button" className="player__full-screen">
+          <button type="button" className="player__full-screen" data-state="go-fullscreen">
             <svg viewBox="0 0 27 27" width="27" height="27">
               <use xlinkHref="#full-screen"></use>
             </svg>
